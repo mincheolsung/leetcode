09169d7cd085ab04041c5b89a4770fc71a142c6b
Mincheol Sung <mincheol@vt.edu>
2022-08-01 00:47:36 -0400

    Time: 8 ms (13.33%), Space: 5.6 MB (86.67%) - LeetHub

---
+++ 980-unique-paths-iii/980-unique-paths-iii.c
@@ -0,0 +1,56 @@
+void dfs(int **orig, int **grid, int gridSize, int *gridColSize, int i, int j, int *cnt) {
+    if (i == -1 || j == -1 || i == gridSize || j == gridColSize[0]) {
+        return;
+    }
+
+    if (grid[i][j] == -1) {
+        return;
+    }
+    
+    if (grid[i][j] == 2) {
+        for (int ii = 0; ii < gridSize; ii++) {
+            for (int jj = 0; jj < gridColSize[0]; jj++) {
+                if (grid[ii][jj] == 0) {
+                    return;
+                }       
+            }
+        }
+        (*cnt)++;
+
+        return;
+    }
+
+    grid[i][j] = -1;
+
+    dfs(orig, grid, gridSize, gridColSize, i-1, j, cnt);
+    dfs(orig, grid, gridSize, gridColSize, i, j-1, cnt);
+    dfs(orig, grid, gridSize, gridColSize, i+1, j, cnt);
+    dfs(orig, grid, gridSize, gridColSize, i, j+1, cnt);
+    
+    grid[i][j] = 0;
+}
+
+int uniquePathsIII(int** grid, int gridSize, int* gridColSize){
+    int **grid_copy = calloc(gridSize, sizeof(int*));
+    for (int i = 0; i < gridSize; i++) {
+        grid_copy[i] = calloc(gridColSize[0], sizeof(int));
+    }
+    
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridColSize[0]; j++) {   
+            grid_copy[i][j] = grid[i][j];
+        }
+    }
+    
+    int result = 0;
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridColSize[0]; j++) {
+            if (grid[i][j] == 1) {
+                dfs(grid, grid_copy, gridSize, gridColSize, i, j, &result);
+                return result;
+            }       
+        }
+    }
+    
+    return result;
+}
