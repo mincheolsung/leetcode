55db75896e5220c11bf3bfd3444bd2eb44a7bd6c
Mincheol Sung <mincheol@vt.edu>
2022-08-01 22:56:50 -0400

    Time: 51 ms (71.43%), Space: 8.6 MB (59.52%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.c
@@ -0,0 +1,26 @@
+int kthSmallest(int** matrix, int matrixSize, int* matrixColSize, int k) {
+    int left,right,mid,cnt,i,j;
+    
+    left = matrix[0][0];
+    right = matrix[matrixSize-1][matrixSize-1];
+
+    while (left < right) {
+        mid = left + (right-left)/2;
+        cnt = 0;
+        j = matrixSize - 1;
+        for (i = 0; i < matrixSize; i++) {
+            while (j >= 0 && matrix[i][j] > mid) {
+                --j;
+            }
+            cnt += j + 1;
+        }
+        
+        if (cnt < k) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return right;
+}
