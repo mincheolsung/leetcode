2aa49c02c5e5ea31a4e81f30f47e4c1f7ad73741
Mincheol Sung <mincheol@vt.edu>
2022-08-06 23:05:51 -0400

    Time: 10 ms (100.00%), Space: 15.2 MB (100.00%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.c
@@ -0,0 +1,28 @@
+#define _a 0
+#define _e 1
+#define _i 2
+#define _o 3
+#define _u 4
+#define MOD(a) ((a)%1000000007ULL)
+
+unsigned long long countVowelPermutation(int n) {
+    int idx;    
+    int result;
+    unsigned long long *dp[5];
+
+    for (idx = 0; idx < 5; idx++) {
+        dp[idx] = calloc(n+1, sizeof(unsigned long long));
+        dp[idx][1] = 1;
+    }
+
+    for (idx = 2; idx <= n; idx++) {     
+        dp[_a][idx] = MOD(dp[_e][idx-1] + dp[_i][idx-1] + dp[_u][idx-1]);
+        dp[_e][idx] = MOD(dp[_a][idx-1] + dp[_i][idx-1]);   
+        dp[_i][idx] = MOD(dp[_e][idx-1] + dp[_o][idx-1]);   
+        dp[_o][idx] = MOD(dp[_i][idx-1]);   
+        dp[_u][idx] = MOD(dp[_i][idx-1] + dp[_o][idx-1]);
+    }
+
+    result = (int)MOD(dp[_a][n] + dp[_e][n] + dp[_i][n] + dp[_o][n] + dp[_u][n]);
+    return result;
+}
