9cbb66a4a2853766eab119b400ec34d7919be198
Mincheol Sung <mincheol@vt.edu>
2022-08-07 22:59:51 -0400

    Time: 173 ms (44.36%), Space: 6.3 MB (48.87%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.c
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.c
@@ -1,12 +1,9 @@
 #define max(a,b) (a>b?a:b)
-#define min(a,b) (a<b?a:b)
 
-int lengthOfLIS(int* nums, int n){
-    int i,j;
-    int max_lis;
-    int *lis;
-    
-    lis = malloc(n*sizeof(int));
+int lengthOfLIS(int* nums, int n) {
+    int i, j;
+    int result;
+    int *lis = malloc(n*sizeof(int));
     if (lis == NULL) {
         return 0;
     }
@@ -15,15 +12,17 @@ int lengthOfLIS(int* nums, int n){
         lis[i] = 1;
     }
     
-    max_lis = lis[0];
+    result = 1;
     for (i = 1; i < n; i++) {
         for (j = 0; j < i; j++) {
             if (nums[j] < nums[i] && lis[j] + 1 > lis[i]) {
                 lis[i] = lis[j] + 1;
-            }   
+            }
         }
-        max_lis = max(max_lis, lis[i]);
+        
+        result = max(result, lis[i]);
     }
     
-    return max_lis;
+    return result;
 }
+
