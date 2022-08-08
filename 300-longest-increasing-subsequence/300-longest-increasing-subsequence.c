664f3f326390eb9b4a2285e90e60a5df45deefea
Mincheol Sung <mincheol@vt.edu>
2022-08-08 19:42:04 -0400

    Time: 8 ms (93.23%), Space: 6.2 MB (69.17%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.c
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.c
@@ -1,5 +1,6 @@
 #define max(a,b) (a>b?a:b)
 /*
+// Recursive
 int helper(int* nums, int n, int *result) {
     int current;
     int temp;
@@ -24,8 +25,8 @@ int lengthOfLIS(int* nums, int n) {
     
     return result;
 }
-*/
 
+// Dynamic programming
 int lengthOfLIS(int* nums, int n) {
     int i, j;
     int LIS[n];
@@ -48,4 +49,47 @@ int lengthOfLIS(int* nums, int n) {
     }
     
     return result;
+}
+*/
+void replace(int*sub, int len, int target) {
+    if (len == 1) {
+        if (target <= sub[0]) {
+            sub[0] = target;
+            return;
+        }
+    }
+
+    for (int i = 0; i < len-1; i++) {
+        if (sub[i] == target) {
+            return;
+        }
+
+        if (sub[i] < target && target <= sub[i+1]) {
+            sub[i+1] = target;
+            return;
+        }
+    }
+}
+
+int lengthOfLIS(int* nums, int n) {
+    int sub[n];
+    int len = 0;
+    int i;
+    
+    len = 1;
+    sub[0] = nums[0];
+    for (i = 1; i < n; i++) {
+        if (sub[len-1] < nums[i]) {
+            sub[len] = nums[i];
+            len++;
+        } else {
+            int k = 0;
+            while (sub[k] < nums[i]) {
+                k++;
+            }
+            sub[k] = nums[i];
+        }
+    }
+
+    return len;
 }
