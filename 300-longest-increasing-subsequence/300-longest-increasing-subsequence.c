2c767852aa0f021ddd09f11fb67f6000085702b8
Mincheol Sung <mincheol@vt.edu>
2022-08-08 16:19:43 -0400

    Time: 175 ms (42.10%), Space: 6.2 MB (48.87%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.c
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.c
@@ -1,28 +1,51 @@
 #define max(a,b) (a>b?a:b)
+/*
+int helper(int* nums, int n, int *result) {
+    int current;
+    int temp;
+        
+    current = 1;
+    for (int i = 1; i < n; i++) {
+        temp = helper(nums, i, result);
+        if (nums[i-1] < nums[n-1] && temp + 1 > current) {
+            current = temp + 1;
+        }
+    }
+    
+    *result = max(*result, current);
+    
+    return current;
+}
+
+int lengthOfLIS(int* nums, int n) {
+    int result = 1;
+    
+    helper(nums, n, &result);
+    
+    return result;
+}
+*/
 
 int lengthOfLIS(int* nums, int n) {
     int i, j;
-    int result;
-    int *lis = malloc(n*sizeof(int));
-    if (lis == NULL) {
-        return 0;
+    int LIS[n];
+    int result = 1;
+
+    for (i = 0; i < n; i++) {
+        LIS[i] = 1;
+    }
+    
+    for (i = 1; i < n; i++) {
+        for (j = 0; j < i; j++) {
+            if (nums[j] < nums[i] && LIS[j] + 1 > LIS[i]) {
+                LIS[i] = LIS[j] + 1;
+            }
+        }
     }
     
     for (i = 0; i < n; i++) {
-        lis[i] = 1;
-    }
-    
-    result = 1;
-    for (i = 1; i < n; i++) {
-        for (j = 0; j < i; j++) {
-            if (nums[j] < nums[i] && lis[j] + 1 > lis[i]) {
-                lis[i] = lis[j] + 1;
-            }
-        }
-        
-        result = max(result, lis[i]);
+        result = max(result, LIS[i]);
     }
     
     return result;
 }
-
