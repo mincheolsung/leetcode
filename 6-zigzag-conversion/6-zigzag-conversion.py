cb266451cc3758c29f0ac01d2286ff2f52db92f1
Mincheol Sung <mincheol@vt.edu>
2022-08-10 21:57:17 -0400

    Time: 3118 ms (5.04%), Space: 20.3 MB (5.08%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        
+        if numRows == 1:
+            return s
+        
+        pattern = [["0" for i in xrange(len(s))] for i in xrange(numRows)]
+        
+        
+        r = -1
+        c = 0
+        i = 0
+        while i < len(s):
+            r += 1
+            pattern[r][c] = s[i];
+            i += 1
+            if r == numRows-1:
+                while r > 0 and i < len(s):
+                    r -= 1
+                    c += 1
+                    pattern[r][c] = s[i];
+                    i += 1
+
+        result = ""
+        for i in range(0, numRows):
+            for j in range(0, len(s)):
+                if pattern[i][j] != "0":
+                    result += pattern[i][j]
+        
+        return result
