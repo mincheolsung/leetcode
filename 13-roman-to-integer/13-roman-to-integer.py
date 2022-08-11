b1076e7104591550000a3085eba0a84d7471d002
Mincheol Sung <mincheol@vt.edu>
2022-08-11 17:01:11 -0400

    Time: 67 ms (42.45%), Space: 13.5 MB (30.90%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -4,50 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        result = 0
-        i = len(s)-1
+
+        roman = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
         
-        while i >= 0:
-            if s[i] == 'I':
-                result+=1
-            elif s[i] == 'V':
-                if i != 0 and s[i-1] == 'I':
-                    result+=4
-                    i-=1
-                else:
-                    result+=5
-            elif s[i] == 'X':
-                if i != 0 and s[i-1] == 'I':
-                    result+=9
-                    i-=1
-                else:
-                    result+=10
-            elif s[i] == 'L':
-                if i != 0 and s[i-1] == 'X':
-                    result+=40
-                    i-=1
-                else:
-                    result+=50
-            elif s[i] == 'C':
-                if i != 0 and s[i-1] == 'X':
-                    result+=90
-                    i-=1
-                else:
-                    result+=100
-            elif s[i] == 'D':
-                if i != 0 and s[i-1] == 'C':
-                    result+=400
-                    i-=1
-                else:
-                    result+=500
-            elif s[i] == 'M':
-                if i != 0 and s[i-1] == 'C':
-                    result+=900
-                    i-=1
-                else:
-                    result+=1000    
-            i-=1
-            
+        result = 0
+        prev = 0
+        
+        for c in s:
+            result += roman[c] if prev >= roman[c] or prev == 0 else roman[c] - prev - prev
+            prev = roman[c]
+
         return result
             
                 
