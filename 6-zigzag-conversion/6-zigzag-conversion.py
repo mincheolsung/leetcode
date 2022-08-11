9d6666c019e5890d4b465bba4e1ea91f299f007b
Mincheol Sung <mincheol@vt.edu>
2022-08-10 22:17:57 -0400

    Time: 46 ms (93.04%), Space: 13.8 MB (32.37%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -9,26 +9,17 @@ class Solution(object):
         if numRows == 1:
             return s
         
-        pattern = [["0" for i in range(len(s))] for i in range(numRows)]
-
-        r = -1
-        c = 0
-        i = 0
-        while i < len(s):
-            r += 1
-            pattern[r][c] = s[i];
-            i += 1
-            if r == numRows-1:
-                while r > 0 and i < len(s):
-                    r -= 1
-                    c += 1
-                    pattern[r][c] = s[i];
-                    i += 1
-
-        result = ""
-        for i in range(0, numRows):
-            for j in range(0, len(s)):
-                if pattern[i][j] != "0":
-                    result += pattern[i][j]
+        result = ['']*numRows
         
-        return result
+        index = 0
+        step = 1
+        
+        for c in s:
+            result[index]+=c
+            if index == 0:
+                step = 1
+            elif index == numRows-1:
+                step = -1
+            index += step
+            
+        return ''.join(result)
