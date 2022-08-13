bf9fac54b1dd0678c086450d733cf7d42b5a2720
Mincheol Sung <mincheol@vt.edu>
2022-08-12 21:27:46 -0400

    Time: 2 ms (64.26%), Space: 5.5 MB (82.45%) - LeetHub

--- 50-powx-n/50-powx-n.c
+++ 50-powx-n/50-powx-n.c
@@ -1,8 +1,21 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        return x**n
+double myPow2(double x, int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    double result = myPow2(x, n/2);
+    if (n%2 == 0) {
+        return result*result;
+    } else {
+        return result*result*x;        
+    }
+}
+
+double myPow(double x, int n) {
+    long long N = n;
+    if (N < 0) {
+        x = 1/x;
+        N = -1*N;
+    }
+    return myPow2(x,N);
+}
