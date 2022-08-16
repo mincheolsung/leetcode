3c4746f8acf9450d91b12aea29cd3e1cd5e6ca43
Mincheol Sung <mincheol@vt.edu>
2022-08-15 20:13:17 -0400

    Time: 121 ms (31.07%), Space: 21.1 MB (75.73%) - LeetHub

--- 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.py
+++ 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.py
@@ -1,28 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Codec:
 
     def serialize(self, root):
-        vals = []
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        result = []
+        
+        def pre_order(root):
+            if root:
+                result.append(root.val)
+                pre_order(root.left)
+                pre_order(root.right)
+        
+        pre_order(root)
+        return ' '.join(str(val) for val in result)
 
-        def preOrder(node):
-            if node:
-                vals.append(node.val)
-                preOrder(node.left)
-                preOrder(node.right)
-
-        preOrder(root)
-
-        return ' '.join(map(str, vals))
-
-    # O( N ) since each val run build once
     def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
         vals = collections.deque(int(val) for val in data.split())
-
-        def build(minVal, maxVal):
+        
+        def bst(minVal, maxVal):
             if vals and minVal < vals[0] < maxVal:
                 val = vals.popleft()
-                node = TreeNode(val)
-                node.left = build(minVal, val)
-                node.right = build(val, maxVal)
-                return node
+                root = TreeNode(val)
+                root.left = bst(minVal, val)
+                root.right = bst(val, maxVal)
+                return root
 
-        return build(float('-infinity'), float('infinity'))
+        return bst(float('-infinity'), float('infinity'))            
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
