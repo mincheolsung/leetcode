5b6e0de02e4b900031446cc3287b0b296c27fd32
Mincheol Sung <mincheol@vt.edu>
2022-08-17 21:19:38 -0400

    Time: 846 ms (60.91%), Space: 26.7 MB (91.82%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minSetSize(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        count = collections.Counter(arr)
+        freq = count.values()
+        freq.sort(reverse=True)
+        
+        length = sum(freq)
+        target = sum(freq)/2
+        i = 0
+        result = 0
+        while i < len(freq):
+            length -= freq[i]
+            if length > target:
+                result+=1
+                i+=1
+            else:
+                result+=1
+                return result
+
+        return result
