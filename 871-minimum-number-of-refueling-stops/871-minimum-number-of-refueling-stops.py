615f89f576df8e87dded869a8303b1e232c46e3a
Mincheol Sung <mincheol@vt.edu>
2022-08-20 17:28:36 -0400

    Time: 864 ms (27.27%), Space: 13.6 MB (81.82%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
@@ -1,12 +1,24 @@
 class Solution(object):
     def minRefuelStops(self, target, startFuel, stations):
-        dp = [startFuel] + [0] * len(stations)
-        for i, (location, capacity) in enumerate(stations):
-            for t in xrange(i, -1, -1):
-                if dp[t] >= location:
-                    dp[t+1] = max(dp[t+1], dp[t] + capacity)
+        """
+        :type target: int
+        :type startFuel: int
+        :type stations: List[List[int]]
+        :rtype: int
+        """
+        
+        num_stations = len(stations)
+        dp = [0]*(num_stations+1)
+        dp[0] = startFuel
 
-        for i, d in enumerate(dp):
-            if d >= target:
+        for i, [position, fuel] in enumerate(stations):
+            for j in range(i, -1, -1):
+                if dp[j] >= position:
+                    dp[j+1] = max(dp[j+1], dp[j] + fuel)
+
+        for i in range(len(dp)):
+            if dp[i] >= target:
                 return i
+
         return -1
+            
