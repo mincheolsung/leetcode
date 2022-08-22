b4b34ac5f11b29bdccdb6da9836e7b96817ab7c4
Mincheol Sung <mincheol@vt.edu>
2022-08-21 20:35:39 -0400

    Time: 26 ms (65.99%), Space: 13.5 MB (38.24%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+    
+  
+        return n > 0 and log(n,2) % 2 == 0
+        
+        
