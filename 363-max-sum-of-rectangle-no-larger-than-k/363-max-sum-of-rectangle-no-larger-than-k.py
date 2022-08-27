6d76a20f5c96f6958c00b9097857c717215d9a5d
Mincheol Sung <mincheol@vt.edu>
2022-08-27 19:27:01 -0400

    Time: 3500 ms (65.96%), Space: 14.7 MB (91.49%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
@@ -1,58 +1,29 @@
 class Solution:
     def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
-        '''
-        def findSum(colSum: List[int], k: int) -> int:
-            sortedSet = [0]
-            currentSum = 0
-            maxSum = -sys.maxsize - 1
-
-            for s in colSum:
-                currentSum += s
-                left = bisect.bisect_left(sortedSet, currentSum-k)
-                if left < len(sortedSet):
-                    maxSum = max(maxSum, currentSum - sortedSet[left])
-                bisect.insort(sortedSet, currentSum)
-                
-            return maxSum
-
         if not matrix:
             return 0
-        
-        sizeRow = len(matrix)
-        sizeCol = len(matrix[0])
-        result = -sys.maxsize-1
+    
+        rowSize = len(matrix)
+        colSize = len(matrix[0])
+        result = float('-inf')
 
-        for i in range(sizeCol):
-            colSum = [0]*sizeRow
-            for j  in range(i, sizeCol):
-                for r in range(sizeRow):
-                    colSum[r] += matrix[r][j]
+        for i in range(colSize):
+            sums = [0 for _ in range(rowSize)]
+            for j in range(i, colSize):
+                for row in range(rowSize):
+                    sums[row] += matrix[row][j]
                     
-            result = max(result, findSum(colSum, k))
-            
-        return result
-        '''
-        if not matrix:
-            return 0
-        
-        res = float('-inf')
-        rows, columns = len(matrix), len(matrix[0])
-        for i in range(columns):
-            sums = [0 for _ in range(rows)]
-            for j in range(i, columns):
-                for r in range(rows):
-                    sums[r] += matrix[r][j]
-
-                # find the largest sum of a subarray which is no more than K
                 sortedSet = [0]
-                currentSum, maxSum = 0, float('-inf')
+                curSum = 0
+                tempMax = float('-inf')
                 for item in sums:
-                    currentSum += item
-                    left = bisect.bisect_left(sortedSet, currentSum - k)
-                    if left < len(sortedSet):
-                        maxSum = max(maxSum, currentSum - sortedSet[left])
-                    bisect.insort(sortedSet, currentSum)
+                    curSum += item
+                    index = bisect.bisect_left(sortedSet, curSum - k)
+                    if index < len(sortedSet):
+                        tempMax = max(tempMax, curSum - sortedSet[index])
+                    bisect.insort(sortedSet, curSum)
 
-                res = max(res, maxSum)
+                result = max(result, tempMax)
 
-        return res
+        return result
+                
