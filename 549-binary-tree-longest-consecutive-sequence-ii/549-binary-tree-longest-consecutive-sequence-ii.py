edcdbe80aff91ae20dafe2a878ca7fb11ef49c58
Mincheol Sung <mincheol@vt.edu>
2022-08-27 20:16:34 -0400

    Time: 49 ms (98.18%), Space: 16.4 MB (43.10%) - LeetHub

--- 549-binary-tree-longest-consecutive-sequence-ii/549-binary-tree-longest-consecutive-sequence-ii.py
+++ 549-binary-tree-longest-consecutive-sequence-ii/549-binary-tree-longest-consecutive-sequence-ii.py
@@ -22,14 +22,14 @@ class Solution:
                 left = dfs(root.left)
                 if root.left.val + 1 == root.val:
                     ascending = max(ascending, left[0] + 1)
-                elif root.left.val - 1 == root.val:
+                if root.left.val - 1 == root.val:
                     descending = max(descending, left[1] + 1)
     
             if root.right:
                 right = dfs(root.right)
                 if root.right.val + 1 == root.val:
                     ascending = max(ascending, right[0] + 1)
-                elif root.right.val -1 == root.val:
+                if root.right.val -1 == root.val:
                     descending = max(descending, right[1] + 1)
 
             maxRes = max(maxRes, ascending + descending - 1)
