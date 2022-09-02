9ac21cc331defcd908e6b3a41f0e610210493d38
Mincheol Sung <mincheol@vt.edu>
2022-09-02 18:03:29 -0400

    Time: 6491 ms (66.88%), Space: 14.1 MB (55.34%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -7,10 +7,19 @@ class Solution:
         result = 0
         
         for i in range(n-2):
+            if i > 1 and nums[i] == nums[i-1]:
+                continue
+            
             left = i+1
             right = n-1
             diff = 0
             while left < right:
+                #while left+1 < right and left-1 >= 0 and nums[left] == nums[left-1]:
+                #    left+=1
+                    
+                #while left < right-1 and right+1 < n and nums[right] == nums[right+1]:
+                #    right-=1
+                
                 s = nums[i] + nums[left] + nums[right]
                 diff = abs(target-s)
 
