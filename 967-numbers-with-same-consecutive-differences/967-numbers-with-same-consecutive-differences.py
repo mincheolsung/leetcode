607ea8aa6725f6c31ea807be8baf4cd6e664ab26
Mincheol Sung <mincheol@vt.edu>
2022-09-02 20:29:28 -0400

    Time: 132 ms (5.10%), Space: 14.1 MB (93.88%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,36 +1,28 @@
-class Solution(object):
-    def numsSameConsecDiff(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
         
-        digits = ["0","1","2","3","4","5","6","7","8","9"]
-
         result = []
-
-        def backtrack(index, num):
-            if index == n:
-                if num[0] != "0":
-                    for i in range(0,n-1):
-                        if abs(int(num[i])-int(num[i+1])) != k:
-                            return
-                    result.append(int(num))
+        def backtrack(temp: List, i: int):
+            if len(temp) == n:
+                result.append(int("".join(temp[:])))
+                temp = []
                 return
-             
-            for i in range(0, 10):
-                if len(num) > 0:
-                    if abs(int(num[-1]) - int(digits[i])) != k:
-                        continue
-    
-                num += digits[i]
-                backtrack(index+1, num)
-                num = num[:-1]
 
-        backtrack(0,"")
+            for digit in range(10):
+                if i > 0 and abs(digit - int(temp[i-1])) != k: 
+                    continue
+                if i == 0 and digit == 0:
+                    continue
+                temp.append(str(digit))
+                backtrack(temp, i+1)
+                temp.pop()
+        
+        backtrack([],0)
+        
         
         return result
+                    
+                    
+                
         
-            
             
