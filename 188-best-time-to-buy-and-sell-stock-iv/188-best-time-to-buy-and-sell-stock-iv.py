8e04e64380da2b7ef56e549e89b3fd3f75d434b0
Mincheol Sung <mincheol@vt.edu>
2022-09-17 00:44:24 -0400

    Time: 730 ms (5.87%), Space: 16.5 MB (44.08%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -5,15 +5,14 @@ class Solution:
 
         for i in range(n+1):
             dp[i][0] = 0
-    
-        for i in range(1,n+1):
-            for j in range(1,2*k+1):
+
+        for i in range(1, n+1):
+            for j in range(1, 2*k+1):
                 if j%2:
                     dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] - prices[i-1])
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] + prices[i-1])
-                    
-        
+
         answer = float('-inf')
         for i in range(k+1):
             answer = max(answer, dp[n][2*i])
