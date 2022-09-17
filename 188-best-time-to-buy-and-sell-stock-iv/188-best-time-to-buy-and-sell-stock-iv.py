ab9156637dd874a53bd540c1fd2659879c216ef7
Mincheol Sung <mincheol@vt.edu>
2022-09-17 00:21:30 -0400

    Time: 647 ms (8.10%), Space: 16.5 MB (44.08%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,20 +1,21 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[float('-inf')]*(n+1) for _ in range(2*k+1)]
+        dp = [[float('-inf')]*(2*k+1) for _ in range(n+1)]
 
         for i in range(n+1):
-            dp[0][i] = 0
+            dp[i][0] = 0
     
-        for i in range(1,2*k+1):
-            for j in range(1,n+1):
-                if i%2:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j-1] - prices[j-1])
+        for i in range(1,n+1):
+            for j in range(1,2*k+1):
+                if j%2:
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] - prices[i-1])
                 else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j-1] + prices[j-1])
-            
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] + prices[i-1])
+                    
+        
         answer = float('-inf')
         for i in range(k+1):
-            answer = max(answer, dp[2*i][n])
-    
+            answer = max(answer, dp[n][2*i])
+
         return answer
