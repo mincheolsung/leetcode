a2fb9e602de232b064b2661cdc1adcd2ca1d7c70
Mincheol Sung <mincheol@vt.edu>
2022-09-23 19:48:56 -0400

    Time: 32 ms (93.20%), Space: 14 MB (31.58%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,36 +1,31 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        if len(digits) is 0:
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
             return []
-    
-        dict = {}
-        dict[2] = {'a','b','c'}
-        dict[3] = {'d','e','f'}
-        dict[4] = {'g','h','i'}
-        dict[5] = {'j','k','l'}
-        dict[6] = {'m','n','o'}
-        dict[7] = {'p','q','r','s'}
-        dict[8] = {'t','u','v'}
-        dict[9] = {'w','x','y','z'}
+
+        dict = {
+            "2":("a","b","c"),
+            "3":("d","e","f"),
+            "4":("g","h","i"),
+            "5":("j","k","l"),
+            "6":("m","n","o"),
+            "7":("p","q","r","s"),
+            "8":("t","u","v"),
+            "9":("w","x","y","z")
+        }
+        
+        def backtrack(n: int, temp: List[str]):
+            if n == len(digits):
+                result.append("".join(temp))
+                return
+
+            for alphabet in dict[digits[n]]:
+                #if alphabet not in temp:
+                temp.append(alphabet)
+                backtrack(n+1, temp)
+                temp.pop()
 
         result = []
-        
-        def backtrack(index, temp):
-            if index == len(digits):
-                result.append(str(temp))
-                return
-            
-            for digit in digits[index]:
-                for s in dict[int(digit)]:
-                    temp += s
-                    backtrack(index+1, temp)
-                    temp = temp[:-1]
-            
-        backtrack(0, "")
-        
+        backtrack(0, [])
         return result
-            
+                    
