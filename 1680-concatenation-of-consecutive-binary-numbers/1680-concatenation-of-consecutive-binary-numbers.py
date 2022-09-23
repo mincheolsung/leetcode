6b4568c4bcf9761a26311ed4210c75df77ccec12
Mincheol Sung <mincheol@vt.edu>
2022-09-22 20:11:16 -0400

    Time: 4381 ms (14.96%), Space: 13.9 MB (62.20%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -1,7 +1,5 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        swift = int(log(n,2)) + 1
-
         result = 1
         for num in range(2, n+1):
             swift = int(log(num,2)) + 1
