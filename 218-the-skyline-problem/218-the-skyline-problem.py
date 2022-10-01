abb3d712570bca2c9b7a593eed5ccbcc1e1f438b
Mincheol Sung <mincheol@vt.edu>
2022-10-01 10:00:09 -0400

    Time: 237 ms (55.61%), Space: 19.8 MB (48.42%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.py
+++ 218-the-skyline-problem/218-the-skyline-problem.py
@@ -11,13 +11,13 @@ class Solution:
         #maxHeight[0]: [current maxHeight, boundary]
         maxHeight = [[0,float('inf')]]
         for [x,h,r] in events:
-            while maxHeight[0][1] <= x: # ditch stale max height
+            while maxHeight[0][1] <= x: # pop out building which is end
                 heappop(maxHeight)
 
-            if h: # update max height
+            if h: # if it is a start of a building, push it into maxHeight since its height can be potentially max height
                 heappush(maxHeight, [h,r])
 
-            if result[-1][1] != -maxHeight[0][0]: # if max height changes
+            if result[-1][1] != -maxHeight[0][0]: # if max height chanes from the previous key point
                 result.append([x, -maxHeight[0][0]])
 
         return result[1:]
