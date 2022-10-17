025140a4684b00894744db43963d3c69b17b9a36
Mincheol Sung <mincheol@vt.edu>
2022-10-16 21:45:21 -0400

    Time: 69 ms (13.98%), Space: 14 MB (11.66%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,13 +1,11 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        cnt = [0]*26
-        
+        frequency = [0]*26
+        counter = 26
         for c in sentence:
             index = ord(c) - ord('a') 
-            cnt[index] += 1
-            
-        for i in range(26):
-            if cnt[i] == 0:
-                return False
-        
-        return True
+            if frequency[index] == 0:
+                counter-=1
+            frequency[index] += 1
+
+        return True if counter == 0 else False
