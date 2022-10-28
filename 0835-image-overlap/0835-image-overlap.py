43ca3be8fd4ee678164b3deb9b15da16f756bd0f
Mincheol Sung <mincheol@vt.edu>
2022-10-27 20:28:08 -0400

    Time: 1440 ms (41.52%), Space: 14.7 MB (43.86%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -3,23 +3,19 @@ class Solution:
         n = len(img1)
         point1 = []
         point2 = []
-        num1 = 0
-        num2 = 0
         seen = defaultdict(int)
 
         for i in range(n):
             for j in range(n):
                 if img1[i][j] == 1:
                     point1.append((i,j))
-                    num1+=1
                 if img2[i][j] == 1:
                     point2.append((i,j))
-                    num2+=1
     
         ans = 0
-        for t1 in point1:
-            for t2 in point2:
-                t3 = (t2[0]-t1[0],t2[1]-t1[1])
-                seen[t3] += 1
-                ans = max(ans, seen[t3])
+        for p1 in point1:
+            for p2 in point2:
+                p3 = (p2[0]-p1[0],p2[1]-p1[1])
+                seen[p3] += 1
+                ans = max(ans, seen[p3])
         return ans
