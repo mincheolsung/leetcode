fb917f110a4805d54328c724dc8c365ca38912a6
Mincheol Sung <mincheol@vt.edu>
2022-10-31 21:07:08 -0400

    Time: 217 ms (87.24%), Space: 14.6 MB (12.94%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -7,24 +7,17 @@ class Solution:
             if r == m:
                 return c
 
-            #if c == -1 or c == n:
-             #   return -1
-
             if grid[r][c] == 1:
                 if c == n-1 or grid[r][c+1] == -1:
                     return -1
             else:
                 if c == 0 or grid[r][c-1] == 1:  
                     return -1
-            
+
             return dfs(r + 1, c + grid[r][c])
 
         result = []
         for c in range(n):
-            loc = dfs(0,c)
-            if loc == -1:
-                result.append(-1)
-            else:
-                result.append(loc)
+            result.append(dfs(0,c))
 
         return result
