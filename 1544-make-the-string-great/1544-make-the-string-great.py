587c7d2a75e752ab6cf5f97f7425e9a6c564026a
Mincheol Sung <mincheol@vt.edu>
2022-11-07 19:23:03 -0500

    Time: 48 ms (79.73%), Space: 13.9 MB (15.06%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -3,4 +3,4 @@ class Solution:
         for i in range(len(s)-1):
             if s[i] != s[i+1] and s[i].lower() == s[i+1].lower():
                 return self.makeGood(s[:i]+s[i+2:])
-        return s
+        return s 
