856ae2dd4dc82448b6982d433f315791a943343a
Mincheol Sung <mincheol@vt.edu>
2022-11-14 20:14:14 -0500

    Time: 67 ms (65.59%), Space: 13.8 MB (56.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,7 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        for num in range(2**16):
-            if num*num > x:
-                return num-1
+        left = 0
+        right = 2**16
+        
+        while left < right:
+            pivot = left + (right - left)//2
+            
+            if pivot*pivot <= x:
+                left = pivot + 1
+            else:
+                right = pivot
+    
+        return left-1
             
         
