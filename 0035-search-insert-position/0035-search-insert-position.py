e011d7818fce2599f8065130487ab5fa07e97446
Mincheol Sung <mincheol@vt.edu>
2022-11-14 19:19:49 -0500

    Time: 118 ms (16.40%), Space: 14.8 MB (39.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,6 +1,6 @@
 class Solution:
+    # bisect_left
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # bisect_right
         left = 0
         right = len(nums)
         
@@ -12,4 +12,18 @@ class Solution:
             else:
                 right = mid
         
-        return left 
+        return left
+    
+    def bisect_right(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)
+        
+        while left < right:
+            mid = left + (right-left)//2
+    
+            if nums[mid] <= target:
+                left = mid+1
+            else:
+                right = mid
+        
+        return left
