907412498336d51de1d2500fb1f72381a2d5abfd
Mincheol Sung <mincheol@vt.edu>
2022-12-02 19:34:17 -0500

    Time: 42 ms (95.58%), Space: 15.3 MB (81.15%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,9 +1,7 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         freq = sorted(Counter(s).items(), key = lambda x:(-x[1],x[0]))
-
         ans = []
         for key,val in freq:
             ans.append(key*val)
-            
         return "".join(ans)
