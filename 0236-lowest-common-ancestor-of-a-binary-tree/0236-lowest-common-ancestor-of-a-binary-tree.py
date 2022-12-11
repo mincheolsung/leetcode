b6bd4ce9e7555a3332b2f22778a70b6f3a396aad
Mincheol Sung <mincheol@vt.edu>
2022-12-11 18:09:28 -0500

    Time: 77 ms (87.87%), Space: 26.2 MB (64.90%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,27 +7,23 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def findPath(root: 'TreeNode', target: 'TreeNode', path: List[int]):
-            nonlocal found
-            path.append(root)
-            if root == target:
-                paths.append(path[:])
-                found = True
-                return
-            if not found and root.left:
-                findPath(root.left, target, path)
-            if not found and root.right:
-                findPath(root.right, target, path)
-            path.pop()
+        def dfs(root: 'TreeNode') -> bool:
+            nonlocal ans
 
-        paths = []
-        found = False
-        findPath(root, p, [])
-        found = False
-        findPath(root, q, [])
-    
-        for i in range(min(len(paths[0]), len(paths[1]))):
-            if paths[0][i] == paths[1][i]:
-                ans = paths[0][i]
+            if not root:
+                return False
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            mid = root == p or root == q
 
+            if left + right + mid >= 2:
+                ans = root
+                
+            return left or right or mid
+
+        ans = None
+        dfs(root)
         return ans
+        
+        
