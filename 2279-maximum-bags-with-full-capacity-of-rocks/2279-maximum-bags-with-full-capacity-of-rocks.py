a18e8f761a0e350b18ed26f3da25882ed2d75a3f
Mincheol Sung <mincheol@vt.edu>
2022-12-26 19:46:57 -0500

    Time: 937 ms (94.41%), Space: 22.2 MB (69.41%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -1,10 +1,7 @@
 class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
         n = len(capacity)
-        need = [capacity[i] - rocks[i] for i in range(n)]
-        need.sort()
-
+        need = sorted([capacity[i] - rocks[i] for i in range(n)])
         for i in range(1, n):
             need[i] = need[i-1]+need[i]
-
         return bisect_right(need, additionalRocks)
