357693a7cb1f8125b770d9377a607e7fe96119ab
Mincheol Sung <mincheol@vt.edu>
2023-01-02 19:46:28 -0500

    Time: 589 ms (18.95%), Space: 14.6 MB (93.05%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -3,14 +3,14 @@ class Solution:
         m = len(strs)
         n = len(strs[0])
         
-        grid = []
-        for i in range(m):
-            grid.append(list(strs[i]))
+        #grid = []
+        #for i in range(m):
+            #grid.append(list(strs[i]))
 
         ans = 0
         for j in range(n):
             for i in range(m-1):
-                if grid[i][j] > grid[i+1][j]:
+                if strs[i][j] > strs[i+1][j]:
                     ans+=1
                     break
         
