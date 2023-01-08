166477a4cdcfefe6c76350de791d2a3864d3a321
Mincheol Sung <mincheol@vt.edu>
2023-01-08 13:18:16 -0500

    Time: 211 ms (48.24%), Space: 44.2 MB (5.02%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        if n == 1:
+            return 1
+        
+        pairs = set()
+        myMap = defaultdict(set)
+        
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                if (j,i) not in pairs:
+                    pairs.add((i,j))
+
+        for i,j in pairs:
+            x1,y1 = points[i]
+            x2,y2 = points[j]
+
+            if x2-x1 == 0:
+                myMap[(float('inf'), x1)].add((x1,y1))
+                myMap[(float('inf'), x1)].add((x2,y2))
+            else:
+                myMap[((y2-y1)/(x2-x1), ((y1*x2)-(y2*x1))/(x2-x1))].add((x1,y1))
+                myMap[((y2-y1)/(x2-x1), ((y1*x2)-(y2*x1))/(x2-x1))].add((x2,y2))
+
+        ans = 0
+        for item in myMap.values():
+            ans = max(ans, len(item))
+
+        return ans
