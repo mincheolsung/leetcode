72a2d11bc91c13b30ee8fcbc218c308feeb42e21
Mincheol Sung <mincheol@vt.edu>
2023-01-08 13:23:34 -0500

    Time: 196 ms (51.52%), Space: 44 MB (5.02%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(points)
         if n == 1:
             return 1
-
         pairs = set()
         myMap = defaultdict(set)
 
@@ -14,16 +13,15 @@ class Solution:
                 if (j,i) not in pairs:
                     pairs.add((i,j))
 
-        for i,j in pairs:
-            x1,y1 = points[i]
-            x2,y2 = points[j]
+                    x1,y1 = points[i]
+                    x2,y2 = points[j]
 
-            if x2-x1 == 0:
-                myMap[(float('inf'), x1)].add((x1,y1))
-                myMap[(float('inf'), x1)].add((x2,y2))
-            else:
-                myMap[((y2-y1)/(x2-x1), ((y1*x2)-(y2*x1))/(x2-x1))].add((x1,y1))
-                myMap[((y2-y1)/(x2-x1), ((y1*x2)-(y2*x1))/(x2-x1))].add((x2,y2))
+                    if x2-x1 == 0:
+                        myMap[(float('inf'), x1)].add((x1,y1))
+                        myMap[(float('inf'), x1)].add((x2,y2))
+                    else:
+                        myMap[((y2-y1)/(x2-x1), ((y1*x2)-(y2*x1))/(x2-x1))].add((x1,y1))
+                        myMap[((y2-y1)/(x2-x1), ((y1*x2)-(y2*x1))/(x2-x1))].add((x2,y2))
 
         ans = 0
         for item in myMap.values():
