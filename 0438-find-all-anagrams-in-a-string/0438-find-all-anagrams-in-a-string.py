64638d25800ab865390cc77348f77f752173e943
Mincheol Sung <mincheol@vt.edu>
2023-02-05 13:46:39 -0500

    Time: 7128 ms (11.12%), Space: 15.1 MB (74.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,10 +1,16 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         _p = Counter(p)
+        _s = deque([])
+
         n = len(p)
         ans = []
-        for i in range(len(s)-n+1):
-            if (_p == Counter(s[i:i+n])):
-                ans.append(i)
-                
+        for i in range(len(s)):
+            _s.append(s[i])
+            if len(_s) > n:
+                _s.popleft()
+            
+            if len(_s) == n:
+                if _p == Counter(_s):
+                    ans.append(i-n+1)
         return ans
